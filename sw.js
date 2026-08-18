ee61adaf1a091c0b8527cf9df3e7e974b3ae1e4a
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-18 22:58:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='paid-leave-v5-2-halfday-fix';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.url.includes('supabase.co')) return;
+ e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))
+});
